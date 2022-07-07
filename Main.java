9bc4244403633f7fedd3ac5fb45fd13bd4e1273d
s-yasuoka <migayasu@gmail.com>
2022-07-07 19:05:34 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("名前を入力してください。");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("年齢を入力してください。");
+    String ageString = new java.util.Scanner(System.in).nextLine();
+    int age = ageString
+    
+    System.out.println(age);
+  }
+}
